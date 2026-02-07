1ed6ef271b8762751aa232344ab015dbbc680dc7
Azees181 <ahsansana711@gmail.com>
2026-02-07 20:22:44 +0530

    script.js

--- script.js
+++ script.js
@@ -10,3 +10,14 @@ function moveNo() {
   btn.style.top = Math.random() * 80 + "%";
 
 }
+
+setInterval(() => {
+  const heart = document.createElement("div");
+  heart.className = "heart";
+  heart.innerHTML = "❤️";
+  heart.style.left = Math.random() * 100 + "vw";
+  document.body.appendChild(heart);
+
+  setTimeout(() => heart.remove(), 4000);
+}, 500);
+
